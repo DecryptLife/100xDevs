f969abbdb7583f2be124984d5d9575dd3591e60a
DecryptLife <benson.thomas2602@gmail.com>
2023-06-28 00:17:33 -0500

    To-Do List completed - With Tests

--- assignments/Week-1-assignment-with-tests/01-js/hard/todo-list.js
+++ assignments/Week-1-assignment-with-tests/01-js/hard/todo-list.js
@@ -12,7 +12,37 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.todos = [];
+  }
+  add(todo) {
+    this.todos.push(todo);
+  }
+  remove(indexOfTodo) {
+    if (indexOfTodo < this.todos.length) {
+      this.todos.splice(indexOfTodo, 1);
+    }
+  }
+  update(index, updatedTodo) {
+    if (index < this.todos.length) this.todos[index] = updatedTodo;
+  }
+  getAll() {
+    return this.todos;
+  }
+  get(indexOfTodo) {
+    if (indexOfTodo < this.todos.length) return this.todos[indexOfTodo];
+    else return null;
+  }
+  clear() {
+    this.todos = [];
+  }
 }
 
+const todoList = new Todo();
+todoList.add("Task 1");
+todoList.add("Task 2");
+todoList.add("Task 3");
+
+todoList.remove(1);
+console.log(todoList.getAll());
 module.exports = Todo;
