83393141a09804bc744784b3e35388ac1393a146
DecryptLife <benson.thomas2602@gmail.com>
2023-06-27 23:49:37 -0500

    Solved Palindrome with Tests

--- assignments/Week-1-assignment-with-tests/01-js/medium/palindrome.js
+++ assignments/Week-1-assignment-with-tests/01-js/medium/palindrome.js
@@ -7,7 +7,31 @@
 */
 
 function isPalindrome(str) {
+  var start = 0;
+  var end = str.length - 1;
+
+  str = str.toLowerCase();
+
+  for (var i = 0; i < str.length / 2; i++) {
+    var startCh = str.charAt(start);
+    var endCh = str.charAt(end);
+
+    if (startCh.charCodeAt(0) >= 97 && startCh.charCodeAt(0) <= 122) {
+      if (endCh.charCodeAt(0) >= 97 && endCh.charCodeAt(0) <= 122) {
+        if (str.charAt(start) !== str.charAt(end)) return false;
+
+        start += 1;
+        end -= 1;
+      } else {
+        end = end - 1;
+      }
+    } else {
+      start = start + 1;
+    }
+  }
+
   return true;
 }
 
+console.log(isPalindrome(""));
 module.exports = isPalindrome;
