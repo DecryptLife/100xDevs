082b2d22726914b5c1cf918122902af6f0087935
DecryptLife <benson.thomas2602@gmail.com>
2023-07-18 22:26:16 -0500

    Frontend Todo App - Using Vite

---
+++ frontend-todo-app/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./App.css";
+
+// var todos = [
+//   {
+//     title: "go to gym",
+//     description: "go to gym at 1",
+//     id: 1,
+//   },
+//   {
+//     title: "go to church",
+//     description: "go to church at 12p",
+//     id: 1,
+//   },
+// ];
+
+var todo = {
+  title: "Go to Gym",
+  description: "Go to the gym after Owlspark",
+  id: 1,
+};
+
+function App() {
+  const [todos, setTodos] = useState({
+    title: "Go to Gym",
+    description: "Go to gym after Owlspark program",
+  });
+
+  setInterval(() => {
+    setTodos({
+      title: "Go to church",
+      description: "Go to chruch this Sunday",
+      id: 2,
+    });
+  }, 3000);
+  return (
+    <>
+      <h1>
+        {todos.title}
+        <br></br>
+        {todos.description}
+      </h1>
+    </>
+  );
+}
+
+export default App;
