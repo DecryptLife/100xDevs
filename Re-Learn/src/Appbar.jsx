73069708dfd74cee58b06d26428cfb5fe6a0a1ab
DecryptLife <benson.thomas2602@gmail.com>
2023-07-26 23:17:28 -0500

    Middleware to check if user signed in or not

--- Re-Learn/src/Appbar.jsx
+++ Re-Learn/src/Appbar.jsx
@@ -1,8 +1,23 @@
 // import { useHistory } from "react-router-dom";
 import { Typography } from "@mui/material";
 import { Button } from "@mui/material";
+import { useEffect } from "react";
 function Appbar() {
   // const history = useHistory();
+
+  const path = "http://localhost:3067";
+  useEffect(() => {
+    fetch(`${path}/admin/me`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div
       style={{
