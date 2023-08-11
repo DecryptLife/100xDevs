89fbce6c3f65c6c13312a94fb9ad9f5208c5155c
DecryptLife <benson.thomas2602@gmail.com>
2023-08-11 14:54:17 -0500

    Calculator using TS

---
+++ Typescript/calculator.ts
@@ -0,0 +1,27 @@
+function calculator(first: number, second: number, opeartion: string): number {
+  var ans = 0;
+  switch (opeartion) {
+    case "add":
+      ans = first + second;
+      break;
+    case "div":
+      ans = first / second;
+      break;
+    case "mul":
+      ans = first * second;
+      break;
+    case "sub":
+      ans = first - second;
+      break;
+    default:
+      ans = 0;
+      break;
+  }
+
+  return ans;
+}
+
+var output = calculator(10, 5, "div");
+
+if (output != 0) console.log(output);
+else console.log("Wrong operation mentioned");
