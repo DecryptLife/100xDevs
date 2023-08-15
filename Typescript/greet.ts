6a14c42737d5d860ee3bc24ec6ce2e618b706fd8
DecryptLife <benson.thomas2602@gmail.com>
2023-08-15 01:12:59 -0500

    Understanding Types - ORs and ANDs

--- Typescript/greet.ts
+++ Typescript/greet.ts
@@ -1,15 +1,29 @@
-type PersonInterface = {
-  name: string;
-  age: number;
-};
-
-function greet(person: PersonInterface) {
-  return "Hi Mr." + person.name + " your age is " + person.name;
+interface Circle {
+  radius: number;
 }
 
-console.log(
-  greet({
-    name: "Benson",
-    age: 24,
-  })
-);
+interface Square {
+  side: number;
+}
+
+interface Rectangle {
+  width: number;
+  height: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function renderShape(shape: Shape) {
+  console.log("rendered");
+}
+
+function calculateArea(shape: Shape) {
+  console.log("calculated area");
+}
+
+renderShape({
+  radius: 10,
+  width: 10,
+  height: 10,
+  side: 10,
+});
