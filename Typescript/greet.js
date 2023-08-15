0f83830806b52254b4f6d61fa39f0a5b8e0b03a6
DecryptLife <benson.thomas2602@gmail.com>
2023-08-15 01:07:21 -0500

    Replacing Interfaces with Types

--- Typescript/greet.js
+++ Typescript/greet.js
@@ -1,12 +1,7 @@
-var Person = /** @class */ (function () {
-    function Person(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    Person.prototype.greet = function () {
-        return "Hi Mr." + this.name + " glad you're " + this.age + " years old";
-    };
-    return Person;
-}());
-var Benson = new Person("Benson", 21);
-console.log(Benson.greet());
+function greet(person) {
+    return "Hi Mr." + person.name + " your age is " + person.name;
+}
+console.log(greet({
+    name: "Benson",
+    age: 24,
+}));
--- Typescript/greet.ts
+++ Typescript/greet.ts
@@ -1,22 +1,15 @@
-interface PersonInterface {
+type PersonInterface = {
   name: string;
   age: number;
+};
+
+function greet(person: PersonInterface) {
+  return "Hi Mr." + person.name + " your age is " + person.name;
 }
 
-class Person implements PersonInterface {
-  name: string;
-  age: number;
-
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
-
-  greet() {
-    return "Hi Mr." + this.name + " glad you're " + this.age + " years old";
-  }
-}
-
-const Benson = new Person("Benson", 21);
-
-console.log(Benson.greet());
+console.log(
+  greet({
+    name: "Benson",
+    age: 24,
+  })
+);
