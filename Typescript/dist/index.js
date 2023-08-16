8a8f400bbd67ce8d74e15cdc46fc4446f9c5ed3c
DecryptLife <benson.thomas2602@gmail.com>
2023-08-16 16:55:15 -0500

    Function returns number or string array

--- Typescript/dist/index.js
+++ Typescript/dist/index.js
@@ -3,5 +3,6 @@ function firstEle(arr) {
     if (arr.length > 0)
         return arr[0];
 }
-const ele = firstEle([12, 32, 34, 56, 68]);
-console.log(ele);
+const a = firstEle([12, 32, 34, 56, 68]);
+const b = firstEle(["Benson", "Thomas", "Beena"]);
+console.log(b);
--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,9 +1,9 @@
-type numArray = number[];
+type numArray = number[] | string[];
 
 function firstEle(arr: numArray) {
   if (arr.length > 0) return arr[0];
 }
 
-const ele = firstEle([12, 32, 34, 56, 68]);
-
-console.log(ele);
+const a = firstEle([12, 32, 34, 56, 68]);
+const b = firstEle(["Benson", "Thomas", "Beena"]);
+console.log(b);
