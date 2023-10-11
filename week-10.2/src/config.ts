a77621a137aabe6d92c48810103e521d45986b23
DecryptLife <benson.thomas2602@gmail.com>
2023-10-11 00:19:06 -0500

    running locally

--- week-10.2/src/config.ts
+++ week-10.2/src/config.ts
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://week10.btdcrypt.com/";
-export const NEXT_URL = "http://localhost:3001";
+// export const BASE_URL = "http://week10.btdcrypt.com/";
+export const BASE_URL = "http://localhost:3001";
+export const NEXT_URL = "http://localhost:3002";
